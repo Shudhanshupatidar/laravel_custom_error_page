1a3355e80710efb686ec9343af42e236dcfc3227
Shudhanshu Patidar <shudhanshupatidar@users.noreply.github.com>
2020-12-01 15:54:50 +0530

    Project_Created

---
+++ DemoProject/resources/views/errors/404.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"></script>
+<style>
+       body{
+          margin-top: 150px;
+            background-color: #e2e2e2;
+        }
+</style>
+</head>
+
+<body>
+    <div class="container">
+        <div class="row ">
+            <div class="col-lg-8 col-12 col-sm-10 mx-auto text-center my-5">
+                <h1 class="m-0">404</h1>
+                <h5>Page not found </h5>
+                <p>Visit : <a href="http://coding-monkeys.com/">Coding-monkeys</a></p>
+                <p>The last coding blog that you are going to follow</p>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
